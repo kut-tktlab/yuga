7b8437de7e6694f7871fd5d2e6de2386448fb69a
yuga190378 <gayokunomiko@gmail.com>
2018-11-21 11:41:09 +0900

    ちょい変更

--- ironuki.java
+++ ironuki.java
@@ -3,8 +3,8 @@ import java.io.*;
 import javax.imageio.ImageIO;
 public class ironuki {
     public static void main(String[] args) throws IOException {
-        //File f = new File("humen_rei.png");
-        File f = new File("iro1.png");
+        File f = new File("humen_rei.png");
+        //File f = new File("iro1.png");
         BufferedImage read=ImageIO.read(f);
         int w = read.getWidth(),h = read.getHeight();
         BufferedImage write =
@@ -24,12 +24,12 @@ public class ironuki {
                 //if(r==51 && g==51 && b==50){
                   //r=0;g=0;b=0;
                 //} else {r=255;g=255;b=255;}
-                //if(r>=50 && r<=52 && g>=50 && g<=52 && b>=50 && b<=52){ //境界線処理
-                  //r=0;g=0;b=0;
-                //} else {r=255;g=255;b=255;}
-                if(r==0 && g==0 && b==0){
+                if(r>=50 && r<=52 && g>=50 && g<=52 && b>=50 && b<=52){ //境界線判定(範囲指定)
                   r=0;g=0;b=0;
                 } else {r=255;g=255;b=255;}
+                //if(r==0 && g==0 && b==0){
+                  //r=0;g=0;b=0;
+                //} else {r=255;g=255;b=255;}
                 int rgb = ImageUtility.rgb(r,g,b);
                 write.setRGB(x,y,rgb);
             }
